fa9b4cf98672692bea4d646e980d603f8a1c4827
Joao Goncalves <jp.reisgoncalves@gmail.com>
2019-10-28 23:04:08 +0000

    tests

--- client-app/client-app/Program.cs
+++ client-app/client-app/Program.cs
@@ -1,21 +1,56 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
+using System.Text.Json;
 
 namespace ClientApp
 {
+	internal class DataObject
+	{
+		public string Name { get; set; }
+	}
 	internal static class Program
 	{
 		private static void Main(string[] args)
 		{
-			Console.WriteLine("Client-App invoked");
-			Console.WriteLine("Arguments:");
-
-			foreach (var s in args)
+			try
 			{
-				Console.WriteLine(CleanArguments(s));
-			}
+				// put all code in here ...
+				Console.WriteLine("Client-App invoked");
+				Console.WriteLine("Arguments:");
 
-			Console.WriteLine("Press any key to continue...");
-			Console.ReadLine();
+				foreach (var s in args)
+				{
+					var cleanString = CleanArguments(s);
+					Console.WriteLine(cleanString);
+
+					var decoded = System.Web.HttpUtility.UrlDecode(cleanString);
+					Console.WriteLine(decoded);
+
+					//var token = JToken.Parse(decoded);
+					//var json = JObject.Parse((string)token);
+					//Console.WriteLine(json);
+					var byteArray = Convert.FromBase64String(decoded);
+					var jsonBack = Encoding.UTF8.GetString(byteArray);
+					//var accountBack = JsonConvert.DeserializeObject<ExternalAccount>(jsonBack);
+
+					Console.WriteLine(jsonBack);
+
+					var d = JsonSerializer.Deserialize("{name:joao}", typeof(DataObject));
+				}
+
+				Console.WriteLine("Press any key to continue...");
+				Console.ReadLine();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				Console.Write("Press Enter to close window ...");
+				Console.Read();
+			}
 		}
 
 		private static string CleanArguments(string argument)
